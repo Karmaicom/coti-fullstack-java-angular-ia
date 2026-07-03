bca35655ef89a688e0f8ea9a239ccea64ef9b2b1
Karmaicom Rocha <karmaicom@gmail.com>
2026-07-03 10:07:34 -0300

    Implementacao da autenticacao.

--- aula14/angular/web-financas/src/app/autenticar-usuario/autenticar-usuario.ts
+++ aula14/angular/web-financas/src/app/autenticar-usuario/autenticar-usuario.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
@@ -17,6 +17,9 @@ export class AutenticarUsuario {
   //e inicializa-lo por injeção de dependência
   private http = inject(HttpClient);
 
+  //Atributo (signal)
+  mensagemErro = signal<string>('');
+
   formAutenticar = new FormGroup({
     email : new FormControl('', [Validators.required]),
     senha : new FormControl('', Validators.required)
@@ -29,10 +32,16 @@ export class AutenticarUsuario {
     this.http.post('http://localhost:8081/api/v1/usuario/autenticar', this.formAutenticar.value)
         .subscribe({
         next : (response) => { //Capturando se o retorno for sucesso da API
-          console.log('Sucesso!', response);
+          //console.log('Sucesso!', response);
+          //Salvar os dados do usuario autenticado na sessao do navegador
+          sessionStorage.setItem('auth', JSON.stringify(response));
+
+          //Redirecionar para o dashboard do sistema
+          location.href = '/app/dashboard'
         },
         error : (e) => { //Capturando se o retorno for diferente de HTTP do grupo 200
-          console.log("Error: ", e.error);
+          //console.log("Error: ", e.error);
+          this.mensagemErro.set(e.error);
         }
       });
   }
