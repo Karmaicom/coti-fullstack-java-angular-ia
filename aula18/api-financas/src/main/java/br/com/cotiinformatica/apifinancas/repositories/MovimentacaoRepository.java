f6e01079ccaa65ca66bdcab9621d17791bc8e68d
Karmaicom Rocha <karmaicom@gmail.com>
2026-07-03 12:55:37 -0300

    Estereotipagem das classes de repositório.

--- aula18/api-financas/src/main/java/br/com/cotiinformatica/apifinancas/repositories/MovimentacaoRepository.java
+++ aula18/api-financas/src/main/java/br/com/cotiinformatica/apifinancas/repositories/MovimentacaoRepository.java
@@ -2,9 +2,11 @@ package br.com.cotiinformatica.apifinancas.repositories;
 
 import br.com.cotiinformatica.apifinancas.entities.Movimentacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, UUID> {
 
 }
