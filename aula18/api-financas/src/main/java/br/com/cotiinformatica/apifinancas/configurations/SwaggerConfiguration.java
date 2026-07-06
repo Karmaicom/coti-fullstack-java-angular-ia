bdd2724984f77e40162606626f46b2eefcd7d445
Karmaicom Rocha <karmaicom@gmail.com>
2026-07-06 10:55:00 -0300

    Configuração do swagger na aplicação.

---
+++ aula18/api-financas/src/main/java/br/com/cotiinformatica/apifinancas/configurations/SwaggerConfiguration.java
@@ -0,0 +1,44 @@
+package br.com.cotiinformatica.apifinancas.configurations;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Usuários API")
+                        .description("API para gerenciamento de finançasa.")
+                        .version("v1.0.0")
+                        .contact(new Contact()
+                                .name("Karmaicom Rocha")
+                                .email("karmaicom@gmail.com")
+                        )
+                );
+                /*
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName)
+                )
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );*/
+    }
+}
